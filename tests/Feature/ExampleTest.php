33f65ae47ba52a6add5a241445ab1a1dbf81fa5f
Chisom Ejim <ejimchisom@gmail.com>
2022-10-08 23:38:04 +0100

    add test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,19 +3,32 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Task;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
+    use DatabaseTransactions;
 
-        $response->assertStatus(200);
+    public function testTasksCanBeCreated()
+    {
+        $task1 = 'Task 1';
+        $this->json('post', '/task', ['name' => $task1]);
+        $this->assertDatabaseHas('tasks', ['name' => $task1]);
+
+    }
+
+    public function testTasksCanBeDisplayedOnDashboard()
+    {
+        $task1 = 'Task 1';
+        $task2 = 'Task 2';
+        $task3 = 'Task 2';
+        Task::factory()->create(['name' => $task1]);
+        Task::factory()->create(['name' => $task2]);
+        Task::factory()->create(['name' => $task3]);
+
+        $this->get('/')->assertSee($task1)->assertSee($task2)->assertSee($task3);
+
     }
 }
