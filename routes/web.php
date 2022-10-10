9342c1932350cff0fad13ecd3aa83116a264248a
Chisom Ejim <ejimchisom@gmail.com>
2022-10-10 12:19:04 +0100

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use App\Http\Controllers\TaskController;
+use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +21,55 @@ Route::get('/home', function () {
     return view('welcome');
 });
 
+/**
+ * Proper way of writing the code
+ */
+//Route::group(['middleware' => ['web']], function () {
+//    Route::get('/', [TaskController::class, 'index']);               // Show task dashboard
+//    Route::post('/task', [TaskController::class, 'store']);          // Add new task
+//    Route::delete('/task/{task}', [TaskController::class, 'destroy']); // Delete task
+//});
+
+/**
+ * Wrong way of writing the code
+ */
 Route::group(['middleware' => ['web']], function () {
-    Route::get('/', [TaskController::class, 'index']);               // Show task dashboard
-    Route::post('/task', [TaskController::class, 'store']);          // Add new task
-    Route::delete('/task/{task}', [TaskController::class, 'destroy']); // Delete task
+    /**
+     * Show Task Dashboard
+     */
+    Route::get('/', function () {
+        return view('tasks', [
+            'tasks' => Task::query()->orderBy('created_at', 'asc')->get()
+        ]);
+    });
+
+    /**
+     * Add New Task
+     */
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
+    });
+
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{id}', function ($id) {
+        Task::query()->findOrFail($id)->delete();
+
+        return redirect('/');
+    });
 });
